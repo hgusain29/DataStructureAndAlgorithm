9ee73e94c685cc1928e8f2991f13886847056453
hgusain29 <himanshugusain2962@gmail.com>
2023-09-09 15:46:50 +0530

    better approach

--- SelectionSort.cpp
+++ SelectionSort.cpp
@@ -1,20 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
 void SelectionSort(int arr[],int n){
-int temp[n];
-for(int i = 0 ; i < n ; i++ ){
-int min = 0 ;
-    for(int j = 1 ; j < n ; j++){
-        if(arr[min]>arr[j]){
-            min=j;
-            temp[i]=arr[min];
-            arr[min] = INT_MAX;
 
+for(int i = 0 ; i < n ; i++ ){
+int min = i ;
+    for(int j = i+1 ; j < n ; j++){
+        if(arr[min]>arr[j]){
+            min = j ;
         }
     }
+    swap(arr[min],arr[i]);
 }
 for(int i = 0 ; i < n ; i++){
-cout<<temp[i]<<",";
+cout<<arr[i]<<",";
 }
 }
 
